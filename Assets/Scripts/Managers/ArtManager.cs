8484c2075cc81286b3fa5ca8b2cc9071401511fc
Giowany Ribeiro <giowany_100@hotmail.com>
2023-03-28 19:20:13 -0300

    artManager Error

---
+++ Assets/Scripts/Managers/ArtManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EBAC.Core.Singleton;
+
+public class ArtManager : Singleton<ArtManager>
+{
+    public enum ArtType
+    {
+        TYPE_01,
+        TYPE_02,
+        TYPE_03
+    }
+
+    public List<ArtSetup> artSetups;
+
+    public ArtSetup GetArtByType(ArtType artType)
+    {
+        return artSetups.ForEach(i => i.type = artType);
+    }
+
+}
+
+[System.Serializable]
+public class ArtSetup
+{
+    public ArtManager.ArtType type;
+    public GameObject artPart;
+}
